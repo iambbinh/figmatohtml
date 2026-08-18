b36e69216c2eb915ca31c748d492999e40f091ad
Bao Binh <baobinh157.qn@gmail.com>
2026-08-18 22:29:26 +0700

    Popular + Ép dữ liệu

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+// Ép kiểu dữ liệu
+// Xuất thông báo cho người dùng nhập vào số a
+let numberA = prompt("Nhập vào số a:");
+console.log(typeof numberA); // Kiểm tra kiểu dữ liệu của numberA
+let numberB = 5;
+console.log(typeof numberB); // Kiểm tra kiểu dữ liệu của numberB
+// Cộng A và B
+let kq = numberA + numberB;
+console.log("Kết quả của phép cộng là: " + kq); // Xuất kết quả ra console
